afeb480649ddade44fea2adb8243bbb8737ad2c7
Rüdiger Sonderfeld <ruediger@c-plusplus.de>
2012-10-02 00:48:23 +0200

    Query language progress.
    Signed-off-by: Rüdiger Sonderfeld <ruediger@c-plusplus.de>

--- weather-metno-query.el
+++ weather-metno-query.el
@@ -22,6 +22,33 @@
           (setq ret (cons i nil)))))
     ret))
 
+(defun weather-metno~op-each (ops entry)
+  ""
+  (let ((each (cadr (weather-metno-query~get-op :each ops))))
+    (if (not each)
+        entry
+      (if (functionp each)
+          (list 'funcall `(quote ,each) entry)
+        entry))))
+
+(defun weather-metno~op-select (ops entry)
+  "Select according to OPS from ENTRY.
+Implements :select operation."
+  (let ((select (cadr (weather-metno-query~get-op :select ops))))
+    (if (consp select)
+        `(list
+          ,@(mapcar
+             (lambda (i)
+               (weather-metno~op-each
+                ops
+                `(cdr (assq (quote ,i) (cadr ,entry)))))
+             select))
+      (if (symbolp select)
+          (weather-metno~op-each
+           ops
+           `(cdr (assq (quote ,select) (cadr ,entry))))
+        entry))))
+
 (defmacro weather-metno-query (x &rest body)
   "Queries DATA for values at LOCATION for DATE.
 
@@ -52,16 +79,35 @@
                               (setq storage (cons (quote ,symbol) nil))
                               (setq ret (append ret (list storage))))
 
-                            (setcdr storage (append (cdr storage) (cdr entry)))))))
+                            (setcdr storage
+                                    (append (cdr storage)
+                                            (list
+                                             ,(weather-metno~op-select ops 'entry))))))))
                     body2))))))
+       ,@(mapcar
+          (lambda (ops)
+            (let ((symbol (cadr (weather-metno-query~get-op :get ops)))
+                  (reduce (cadr (weather-metno-query~get-op :reduce ops))))
+              (when (functionp reduce)
+                `(let ((storage (assq (quote ,symbol) ret)))
+                   (when storage
+                     (setcdr storage (funcall (quote ,reduce) (cdr storage))))))))
+          body2)
        ret)))
 
+(defun avg (x)
+  (/ (reduce #'+ x)
+     (length x)))
 
 
 (weather-metno-query
  (org-weather-metno~data '(lat lon msl) '(10 1 2012))
 
- :get temperature :select value :each (convert or something) :reduce avg :format "%s°C"
+ :get temperature :select value :each string-to-number :reduce avg :format "%s°C"
  :get windSpeed :select (mps name beaufort) :max mps
  :get precipitation :select value :max-min
  :get pressure :select value :max)
+
+
+(weather-metno~op-select '(:get temperature :select value :each) '((value . "jo")))
+
